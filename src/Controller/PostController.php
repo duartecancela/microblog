ac0fd9166e8c8e8cce2a9c6867d514902178fa2d
Duarte Cancela <duartecancela@gmail.com>
2024-01-07 15:53:34 +0000

    User Registration

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,66 +5,49 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Post;
-use Doctrine\Persistence\ManagerRegistry;
 
+#[Route('/', requirements: ['_locale' => 'en|pl'])]
 class PostController extends AbstractController
 {
-    #[Route('/{_locale?}', methods: ['GET'], name: 'posts.index')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/{_locale}', methods: ['GET'], name: 'posts.index')]
+    public function index(string $_locale = 'en'): Response
     {
-        // $post = $doctrine->getRepository(Post::class)->find(2); // findAll()
-        // // $doctrine->getManager()->remove($post);
-        // // $doctrine->getManager()->flush();
-        // $post->setTitle('New blog title!');
-        // $doctrine->getManager()->flush();
-        // return new Response('Check out this great product: ' . $post->getTitle());
         return $this->render('post/index.html.twig');
     }
 
-    #[Route('/post/new', methods: ['GET', 'POST'], name: 'posts.new')]
-    public function new(ManagerRegistry $doctrine): Response
+    #[Route('/{_locale}/post/new', methods: ['GET', 'POST'], name: 'posts.new')]
+    public function new(): Response
     {
-        // $entityManager = $doctrine->getManager();
-        // $post = new Post();
-        // $post->setTitle('Title');
-        // $post->setContent('Post content');
-        // $post->setCreatedAt(new \DateTimeImmutable());
-        // //tell Doctrine you want to (eventually) save the Product (no queries yet)
-        // $entityManager->persist($post);
-        // //actually executes the queries (i.e. the INSERT query)
-        // $entityManager->flush();
-        // return new Response('Saved new post with id '.$post->getId());
         return $this->render('post/new.html.twig');
     }
 
-    #[Route('/post/{id}/{_locale?}', methods: ['GET'], name: 'posts.show')]
+    #[Route('/{_locale}/post/{id}', methods: ['GET'], name: 'posts.show')]
     public function show($id): Response
     {
         return $this->render('post/show.html.twig');
     }
 
-    #[Route('/post/{id}/edit', methods: ['GET', 'POST'], name: 'posts.edit')]
+    #[Route('/{_locale}/post/{id}/edit', methods: ['GET', 'POST'], name: 'posts.edit')]
     public function edit($id): Response
     {
         // return $this->redirectToRoute('posts.index');
         return $this->render('post/edit.html.twig');
     }
 
-    #[Route('/post/{id}/delete', methods: ['POST'], name: 'posts.delete')]
+    #[Route('/{_locale}/post/{id}/delete', methods: ['POST'], name: 'posts.delete')]
     public function delete($id): Response
     {
         return new Response('delete post from the database');
     }
 
-    #[Route('/posts/user/{id}', methods: ['GET'], name: 'posts.user')]
+    #[Route('/{_locale}/posts/user/{id}', methods: ['GET'], name: 'posts.user')]
     public function user($id): Response
     {
         // return new Response($id);
         return $this->render('post/index.html.twig');
     }
 
-    #[Route('/toggleFollow/{user}', methods: ['GET'], name: 'toggleFollow')]
+    #[Route('/{_locale}/toggleFollow/{user}', methods: ['GET'], name: 'toggleFollow')]
     public function toggleFollow($user): Response
     {
         // return new Response($user);
