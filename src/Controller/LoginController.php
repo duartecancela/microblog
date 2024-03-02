5968128f866c539fd861d3fc2afbf0b54dab1381
Duarte Cancela <duartecancela@gmail.com>
2024-03-02 16:04:45 +0000

    logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -23,5 +23,12 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
 
